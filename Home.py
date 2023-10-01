423ee30950b6357a212b476c3ff052a37b313cd4
KamHaris <141376775+KamHaris@users.noreply.github.com>
2023-10-01 17:19:04 -0400

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+st. set_page_config(layout="wide",page_title="Welcome to Ditchsmart")
+st.write("# Welcome to DitchSmart!")\
+
+
+# In[ ]:
+
+
+
+
