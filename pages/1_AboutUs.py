3a72a1fcacd1f9f0c37d4fc1daf65b2a384d87cf
KamHaris <141376775+KamHaris@users.noreply.github.com>
2023-10-16 02:38:42 +0000

    V2

--- pages/1_AboutUs.py
+++ pages/1_AboutUs.py
@@ -14,7 +14,7 @@ components.html("""<html><!DOCTYPE html>
 
 <h1 style="color: Green">ABOUT US</h1>
 <br>
-<p style ="color: white">Welcome to DitchSmart: Your Decision Helper for Used Appliances
+<p style ="color: black">Welcome to DitchSmart: Your Decision Helper for Used Appliances
 <br> <br>
 We understand how difficult and confusing it can be to decide what to do with your old appliances.
 Do we resell, how can we repair, and can someone just take it off my hands? At DitchSmart, we use
@@ -24,7 +24,7 @@ decisions .</p>
 <br>
 <h1 style="color: Green">Who We Are</h1>
 <br> <br>
-<p style="color: white">We are a dedicated team of data enthusiasts, market analysts, and technology experts on a mission
+<p style="color: black">We are a dedicated team of data enthusiasts, market analysts, and technology experts on a mission
 to simplify the complexities of dealing with ‘ditching’ used appliances. With years of experience in
 market research and data analytics, we are curating this platform to provide accurate and reliable
 information about the resale value, repair estimates, and recycling options for various appliances.</p>
@@ -34,7 +34,7 @@ information about the resale value, repair estimates, and recycling options for 
 <br>
 <h1 style="color: Green">What We Do</h1>
 <br> <br>
-<p style="color: white">At DitchSmart, we aggregate and analyze vast amounts of data from multiple sources and leverage
+<p style="color: black">At DitchSmart, we aggregate and analyze vast amounts of data from multiple sources and leverage
 our analytical tools to provide you with actionable insights. Whether you are an individual looking to
 
 upgrade, small reseller looking to optimize and price your &#39;scratch and dent&#39; inventory, our insights
@@ -44,9 +44,9 @@ and solutions get you the optimal answer and connect you to the right service pr
 <br>
 <h1 style="color: Green">Why Choose Us</h1>
 <br> <br>
-<p style="color: white">
-<li style="color: white">Know the best price to sell your used appliances</li>
-<li style="color: white">Find out how much repairs cost before negotiating with handy professionals</li>
-<li style="color: white">Locate local recycling and donation facilities</li>
+<p style="color: black">
+<li style="color: black">Know the best price to sell your used appliances</li>
+<li style="color: black">Find out how much repairs cost before negotiating with handy professionals</li>
+<li style="color: black">Locate local recycling and donation facilities</li>
 
 <br> </body></html>""", width=1000, height = 1400)
