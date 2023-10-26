c519d27fab300746e72f644f0211092486014bec
KamHaris <141376775+KamHaris@users.noreply.github.com>
2023-10-26 01:01:50 +0000

    --New Updates

---
+++ App_Instructions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+st. set_page_config(layout="wide",page_title="App Instructions")
+st.write("How to Use Our App?")\
+
+
+# In[ ]:
+
+
+
+
